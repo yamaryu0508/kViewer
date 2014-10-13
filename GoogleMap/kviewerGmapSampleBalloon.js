303ab8631fe681485a21de31bfad8a8934d0e10d
Ryu Yamashita <yamaryu0508@gmail.com>
2014-10-13 18:01:34 +0900

    modify RE in function "getElementText()"

--- GoogleMap/kviewerGmapSampleBalloon.js
+++ GoogleMap/kviewerGmapSampleBalloon.js
@@ -109,9 +109,9 @@
     function getElementText(element) {
         if(element){
             if (element.innerText !== undefined) {
-                return element.innerText.replace(/\r?\n((?!\r?\n)|(?=\r?\n\r?\n\r?\n))/g,'').replace(/\s+/g, '');
+                return element.innerText.replace(/\r?\n((?!\r?\n)|(?=\r?\n\r?\n\r?\n))/g,'').replace(/(^\s+)|(\s+$)/g, '');
             } else if (element.textContent !== undefined) {
-                return element.textContent.replace(/\r?\n((?!\r?\n)|(?=\r?\n\r?\n\r?\n))/g,'').replace(/\s+/g, '');
+                return element.textContent.replace(/\r?\n((?!\r?\n)|(?=\r?\n\r?\n\r?\n))/g,'').replace(/(^\s+)|(\s+$)/g, '');
             }
         }else{
             return '';
